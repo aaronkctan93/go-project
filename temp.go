f63c9858d9ae63650d0a0c5e441dd4765cbdcb81
aaronkctan93 <aaronkctan93@gmail.com>
2023-04-07 17:09:57 +0800

    play-my-first-golang

---
+++ temp.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b, c int
+	fmt.Scanf("%d %d %d\n", &a, &b, &c)
+
+	fmt.Printf("%d %d %d\n", a, b, c)
+
+}
+
+// temp := n[5]
+// for i := 5; i >= 2; i-- {
+// 	n[i] = n[i-1]
+// }
+// n[1] = temp
